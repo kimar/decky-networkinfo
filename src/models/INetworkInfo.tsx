8d19ba6993511ce78ef8d9814e2ae9cb45994dc3
Marcus Kida <marcus.kida@bearologics.com>
2023-03-28 16:16:13 +0200

    Add pnpm-lock.yaml

--- src/models/INetworkInfo.tsx
+++ src/models/INetworkInfo.tsx
@@ -1,16 +1,16 @@
-import { IAddrInfo } from "./IAddrInfo"
+import { IAddrInfo } from "./IAddrInfo";
 
 export interface INetworkInfo {
-  ifindex: number
-  ifname: string
-  flags: string[]
-  mtu: number
-  qdisc: string
-  operstate: string
-  group: string
-  txqlen: number
-  link_type: string
-  address: string
-  broadcast: string
-  addr_info: IAddrInfo[]
+  ifindex: number;
+  ifname: string;
+  flags: string[];
+  mtu: number;
+  qdisc: string;
+  operstate: string;
+  group: string;
+  txqlen: number;
+  link_type: string;
+  address: string;
+  broadcast: string;
+  addr_info: IAddrInfo[];
 }
